f926f13699255a6e66eecd89c2de8657dd8bd7de
DebalinaChakrabarti <debalinachakrabarti@gmail.com>
2024-03-27 09:44:06 -0500

    first commit

---
+++ test/src/test/java/test/hello.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver chromeDriver = new ChromeDriver();
+		chromeDriver.get("https://amazon.com");
+
+	}
+
+}
